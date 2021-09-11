75e2397568e88c5bdf090ab0ef804e7a4693efcf
MarcinRozenek <marcin.rozenek@gmail.com>
2021-09-11 07:58:08 +0200

    Project initialization.

---
+++ TaxCalculator.Tests/TaxCalculatorTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace TaxCalculator.Tests
+{
+  public class TaxCalculatorTests
+  {
+    [Fact]
+    public void Test1()
+    {
+      Assert.True(true);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+      Assert.True(false);
+    }
+  }
+}
