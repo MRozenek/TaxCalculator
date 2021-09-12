42658a47eaf3be3e017754e0ddac36557ac12c28
MarcinRozenek <marcin.rozenek@gmail.com>
2021-09-12 10:57:26 +0200

    Using appsettings.json to store tax percentage values.

--- TaxCalculator.Tests/TaxCalculatorTests.cs
+++ TaxCalculator.Tests/TaxCalculatorTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using TaxCalculator.Models;
 using Xunit;
 
@@ -9,15 +10,44 @@ namespace TaxCalculator.Tests
 
     public TaxCalculatorTests()
     {
+      //var config = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
       _cut = new TaxCalculator();
     }
 
     [Fact]
-    public void CalculateTaxIncome_MethodIsProgressiveAndOne5000Payment_ExpectedCalculationResult()
+    public void CalculateTaxIncome_FreeTaxMethodAndTwo5000Payments_ExpectedCalculationResult()
+    {
+      var expectedTaxCalculationResult = 0m;
+      TaxCalculationDetails[] taxIncomes = new TaxCalculationDetails[] {
+        new TaxCalculationDetails { Amount = 5000m, Month = 1, Year = 2021 },
+        new TaxCalculationDetails { Amount = 5000m, Month = 2, Year = 2021 },
+      };
+
+      var taxCalculationResult = _cut.CalculateTaxIncome(taxIncomes, Models.TaxAccountingMethodType.Free);
+
+      Assert.Equal(expectedTaxCalculationResult, taxCalculationResult);
+    }
+
+    [Fact]
+    public void CalculateTaxIncome_FlatTaxMethodAndTwo5001Payments_ExpectedCalculationResult()
+    {
+      var expectedTaxCalculationResult = 1900m;
+      TaxCalculationDetails[] taxIncomes = new TaxCalculationDetails[] {
+        new TaxCalculationDetails { Amount = 5001m, Month = 1, Year = 2021 },
+        new TaxCalculationDetails { Amount = 5001m, Month = 2, Year = 2021 },
+      };
+
+      var taxCalculationResult = _cut.CalculateTaxIncome(taxIncomes, Models.TaxAccountingMethodType.Flat);
+
+      Assert.Equal(expectedTaxCalculationResult, taxCalculationResult);
+    }
+
+    [Fact]
+    public void CalculateTaxIncome_ProgressiveTaxMethodAndOne5000Payment_ExpectedCalculationResult()
     {
       var expectedTaxCalculationResult = 324m;
       TaxCalculationDetails[] taxIncomes = new TaxCalculationDetails[] {
-        new TaxCalculationDetails { Amount = 5000m, Month = 1, Year = 2021 }
+        new TaxCalculationDetails { Amount = 5000m, Month = 1, Year = 2021 },
       };
 
       var taxCalculationResult = _cut.CalculateTaxIncome(taxIncomes, Models.TaxAccountingMethodType.Progressive);
--- TaxCalculator/TaxCalculator.cs
+++ TaxCalculator/TaxCalculator.cs
@@ -1,12 +1,39 @@
-﻿using TaxCalculator.Models;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using TaxCalculator.Models;
 
 namespace TaxCalculator
 {
   public class TaxCalculator : ITaxCalculator
   {
+    private readonly decimal FlatTaxIncomeValueInPercent;
+
+    public TaxCalculator()
+    {
+      var config = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+      FlatTaxIncomeValueInPercent = decimal.Parse(config.GetSection("ApplicationSettings").GetSection("FlatTaxIncomeValueInPercent").Value);
+    }
+
     public decimal CalculateTaxIncome(TaxCalculationDetails[] income, TaxAccountingMethodType methodType)
     {
-      throw new System.NotImplementedException();
+      switch (methodType)
+      {
+        case TaxAccountingMethodType.Free:
+          return 0m;
+        case TaxAccountingMethodType.Flat:
+          var taxIncome = 0m;
+
+          foreach (var item in income)
+          {
+            taxIncome += decimal.Round(item.Amount * FlatTaxIncomeValueInPercent);
+          }
+          return taxIncome;
+        case TaxAccountingMethodType.Progressive:
+          throw new NotImplementedException();
+        case TaxAccountingMethodType.NotSet:
+        default:
+          throw new ArgumentException("Tax accounting method is not set.");
+      }
     }
   }
 }
